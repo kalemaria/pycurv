03dc4d0c396870efbdb3a8e83ac17912ba53cb52
kalemaria <kalemanov@biochem.mpg.de>
2017-07-25 19:17:13 +0200

    Added setup.py for the pysurf_compact package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='polyqIB',
+    version='0.1',
+    packages=['pysurf_compact'],
+    url='',
+    license='',
+    author='Maria Kalemanov, Antonio Martinez',
+    author_email='',
+    description='',
+    install_requires=[
+        "numpy",
+        "scipy",
+        "scikit-image",
+    ],
+)
