692737c14f0b2329000b1c5913b8ea4489d7c947
kalemaria <kalemanov@biochem.mpg.de>
2019-06-17 18:13:31 +0200

    Added Mindboggle's curvature errors calculation for cylinder and adapted the main function to the new output structure.

--- testing/compare_to_mindboggle.py
+++ testing/compare_to_mindboggle.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from os.path import join
+from os import chdir
 
 from errors_calculation import relative_error_scalar
 from test_vector_voting import torus_curvatures_and_directions
@@ -50,6 +51,62 @@ def calculate_curvature_errors_sphere(
     if kappa_2 is not None:
         rel_kappa_2_errors = np.array(
             [relative_error_scalar(true_curvature, x) for x in kappa_2])
+        df['kappa2RelErrors'] = rel_kappa_2_errors
+    if gauss_curv is not None:
+        rel_gauss_curv_errors = np.array(
+            [relative_error_scalar(true_gauss_curvature, x) for x in gauss_curv])
+        df['gauss_curvatureRelErrors'] = rel_gauss_curv_errors
+
+    # Writing all the curvature errors into a second CSV file:
+    df.to_csv(errors_csv_file, sep=';')
+
+
+def calculate_curvature_errors_cylinder(
+        radius, curvatures_csv_file, errors_csv_file):
+    """
+    Function calculating relative curvature errors for a cylinder surface.
+
+    Args:
+        radius (int): radius of the cylinder
+        curvatures_csv_file (string): input curvatures file with columns labeled
+            "kappa1", "kappa2", "mean_curvature" and "gauss_curvature".
+        errors_csv_file (string): output error file with "RelErrors" appended to
+            the aforementioned curvature column labels.
+
+    Returns:
+        None
+    """
+    # read in the curvatures from the CSV file:
+    df = pd.read_csv(curvatures_csv_file, sep=';')
+    mean_curv = df["mean_curvature"].values  # is always there
+    kappa_1 = None
+    kappa_2 = None
+    gauss_curv = None
+    if 'kappa1' in df.columns:
+        kappa_1 = df["kappa1"].values
+    if 'kappa2' in df.columns:
+        kappa_2 = df["kappa2"].values
+    if 'gauss_curvature' in df.columns:
+        gauss_curv = df["gauss_curvature"].values
+
+    # calculate the true curvatures:
+    true_kappa_1 = 1.0 / radius
+    true_kappa_2 = 0.0
+    true_mean_curv = (true_kappa_1 + true_kappa_2) / 2.0
+    true_gauss_curvature = true_kappa_1 * true_kappa_2
+
+    # Calculating relative errors of the curvatures:
+    rel_mean_curv_errors = np.array(
+        [relative_error_scalar(true_mean_curv, x) for x in mean_curv])
+    df = pd.DataFrame()
+    df['mean_curvatureRelErrors'] = rel_mean_curv_errors
+    if kappa_1 is not None:
+        rel_kappa_1_errors = np.array(
+            [relative_error_scalar(true_kappa_1, x) for x in kappa_1])
+        df['kappa1RelErrors'] = rel_kappa_1_errors
+    if kappa_2 is not None:
+        rel_kappa_2_errors = np.array(
+            [relative_error_scalar(true_kappa_2, x) for x in kappa_2])
         df['kappa2RelErrors'] = rel_kappa_2_errors
     if gauss_curv is not None:
         rel_gauss_curv_errors = np.array(
@@ -133,31 +190,29 @@ def calculate_curvature_errors_torus(
 
 
 if __name__ == '__main__':
-    base_fold = "/fs/pool/pool-ruben/Maria/workspace/github/my_tests_output/" \
-                "comparison_to_mindboggle/"
-    for m in [0, 2]:
-        for n in [0.75, 2, 9, 10]:
-            fold = "{}test_surfaces_mindboggle_output_n{}".format(base_fold, n)
-
-            sphere_radius = 10
-            sphere_base = "noisy_sphere_r{}.surface.mindboggle_m{}_n{}".format(
-                sphere_radius, m, n)
-            sphere_curvatures_csv_file = join(
-                fold, "{}_curvatures.csv".format(sphere_base))
-            sphere_errors_csv_file = join(
-                fold, "{}_curvature_errors.csv".format(sphere_base))
-            calculate_curvature_errors_sphere(
-                sphere_radius, sphere_curvatures_csv_file,
-                sphere_errors_csv_file)
-
-            torus_rr = 25
-            torus_csr = 10
-            torus_base = "torus_rr{}_csr{}.surface.mindboggle_m{}_n{}".format(
-                torus_rr, torus_csr, m, n)
-            torus_curvatures_csv_file = join(
-                fold, "{}_curvatures.csv".format(torus_base))
-            torus_errors_csv_file = join(
-                fold, "{}_curvature_errors.csv".format(torus_base))
-            calculate_curvature_errors_torus(
-                torus_rr, torus_csr, torus_curvatures_csv_file,
-                torus_errors_csv_file)
+    test_mindboggle_output_fold = (
+        "/fs/pool/pool-ruben/Maria/workspace/github/my_tests_output/"
+        "comparison_to_mindboggle/test_mindboggle_output/")
+    surface_bases = ['torus_rr25_csr10.surface.', 'smooth_sphere_r10.surface.',
+                 'noisy_sphere_r10.surface.', 'cylinder_r10_h25.surface.']
+    subfolds = ['torus', 'smooth_sphere', 'noisy_sphere', 'cylinder']
+    m = 0
+    ns = range(2, 21, 2)
+    for surface_base, subfold in zip(surface_bases, subfolds):
+        fold = join(test_mindboggle_output_fold, subfold)
+        chdir(fold)
+        for n in ns:
+            out_base = "{}mindboggle_m{}_n{}".format(surface_base, m, n)
+            curvatures_csv_file = join(
+                fold, "{}_curvatures.csv".format(out_base))
+            errors_csv_file = join(
+                fold, "{}_curvature_errors.csv".format(out_base))
+            if out_base.startswith('torus'):
+                calculate_curvature_errors_torus(
+                    25, 10, curvatures_csv_file, errors_csv_file)
+            elif 'sphere' in out_base:
+                calculate_curvature_errors_sphere(
+                    10, curvatures_csv_file, errors_csv_file)
+            else:  # cylinder
+                calculate_curvature_errors_cylinder(
+                    10, curvatures_csv_file, errors_csv_file)
