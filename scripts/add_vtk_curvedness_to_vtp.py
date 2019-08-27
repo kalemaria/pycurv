119324cc007078c34c1b616e839a679c084709c6
kalemaria <kalemanov@biochem.mpg.de>
2019-08-27 16:16:43 +0200

    Added two scripts: 1. adding VTK curvedness to surface VTP file from triangle graph GT file. 2. saving VTP files with triangle graph vertices and lines from GT file.

---
+++ scripts/add_vtk_curvedness_to_vtp.py
@@ -0,0 +1,23 @@
+from graph_tool import load_graph
+import os
+
+from pysurf import TriangleGraph, calculate_curvedness
+from pysurf import pysurf_io as io
+
+# Read in graph GT file
+tg_file = '/fs/gpfs03/lv04/pool/pool-ruben/Maria/4Javier/new_curvature/TCB/' \
+          '180830_TITAN_l2_t2half/TCB_180830_l2_t2half.cER.AVV_rh10.gt'
+tg = TriangleGraph()
+tg.graph = load_graph(tg_file)
+# Read in VTK principle curvatures for each vertex,
+# calculate curvedness and add as a new property
+tg.graph.vp.vtk_curvedness = tg.graph.new_vertex_property("float")
+for v in tg.graph.vertices():
+    v_vtk_kappa_1 = tg.graph.vp.max_curvature[v]
+    v_vtk_kappa_2 = tg.graph.vp.min_curvature[v]
+    v_vtk_curvedness = calculate_curvedness(v_vtk_kappa_1, v_vtk_kappa_2)
+    tg.graph.vp.vtk_curvedness[v] = v_vtk_curvedness
+# Transform the graph to surface and write to a VTP file
+surf = tg.graph_to_triangle_poly(verbose=False)
+surf_file = os.path.splitext(tg_file)[0] + "_VTKcurvedness.vtp"
+io.save_vtp(surf, surf_file)
---
+++ scripts/gt_to_point_and_line_vtp.py
@@ -0,0 +1,14 @@
+from graph_tool import load_graph
+import os
+
+from pysurf import TriangleGraph
+from pysurf import pysurf_io as io
+
+tg_file = '/fs/gpfs03/lv04/pool/pool-ruben/Maria/4Javier/new_curvature/TCB/' \
+          '180830_TITAN_l2_t2half/TCB_180830_l2_t2half.cER.AVV_rh10.gt'
+tg = TriangleGraph()
+tg.graph = load_graph(tg_file)
+poly_verts, poly_lines = tg.graph_to_points_and_lines_polys()
+vtp_files_base = os.path.splitext(tg_file)[0]
+io.save_vtp(poly_verts, vtp_files_base + '.vertices.vtp')
+io.save_vtp(poly_lines, vtp_files_base + '.edges.vtp')
