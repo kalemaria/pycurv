067067a8412cb2ffbb33caddf86c3c220bf57f2b
kalemaria <kalemanov@biochem.mpg.de>
2019-09-03 14:04:49 +0200

    Update version and other details.

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='pycurv',
-    version='1.0',
-    packages=['pycurv', 'scripts', 'testing'],
-    url='',
-    license='',
+    version='1.0.0',
     author='Maria Kalemanov, Antonio Martinez-Sanchez',
-    author_email='',
-    description='',
+    author_email='kalemanov@biochem.mpg.de',
+    description='Reliable estimation of membrane curvature for cryo-electron '
+                'tomography',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/kalemaria/pycurv',
+    packages=find_packages(),
     install_requires=["numpy", "scipy", "scikit-image<0.15", "pandas", "pytest",
                       "matplotlib", "pathlib2", "vtk", "nibabel",
-                      "pathos==0.2.2.1", "networkx==2.2"]
+                      "pathos==0.2.2.1", "networkx==2.2"],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 "
+        "(LGPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='==2.7',
 )
