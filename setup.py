cc99c2d45101eadfa6207a38a0f76697d05e89f7
kalemaria <kalemanov@biochem.mpg.de>
2019-11-25 11:30:48 +0100

    Update PyCurv version and release.
    From 1.0(.0) to 2.0(.0).

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pycurv',
-    version='1.0.0',
+    version='2.0.0',
     author='Maria Kalemanov, Antonio Martinez-Sanchez',
     author_email='kalemanov@biochem.mpg.de',
     description='Reliable estimation of membrane curvature for cryo-electron '
