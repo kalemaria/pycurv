a1c3f62aef534af4c6a7975ad20786526f0a7b8f
kalemaria <kalemanov@biochem.mpg.de>
2020-02-17 15:11:48 +0100

    Fixed sphinx module index generation:
    run sphinx-apidoc -o . ./pycurv
    run sphinx-apidoc -o . ./pycurv_testing
    run sphinx-apidoc -o . ./pycurv_scripts
    uncomment in conf.py: sys.path.insert(0, os.path.abspath('..'))
    re-run make html
    
    Also uncommented rst2pdf.pdfbuilder because no compatible version found.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- General configuration ------------------------------------------------
@@ -32,7 +32,7 @@ import sys
 # ones.
 extensions = ['sphinx.ext.coverage',
               'sphinx.ext.autodoc', 'sphinxcontrib.napoleon',
-              'sphinx.ext.githubpages', 'rst2pdf.pdfbuilder']
+              'sphinx.ext.githubpages']  # , 'rst2pdf.pdfbuilder'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
